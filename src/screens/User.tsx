27d002ff35a6a26d8292971a1b2aacc3e65b1663
Ayandip paul <152761756+Ayandip13@users.noreply.github.com>
2025-08-23 23:59:28 +0530

    feat: implement user fetching from Firestore and update UI

--- src/screens/User.tsx
+++ src/screens/User.tsx
@@ -1,10 +1,42 @@
 import { View, Text, StyleSheet } from 'react-native';
-import React from 'react';
+import React, { useEffect } from 'react';
+import firestore from '@react-native-firebase/firestore';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 const User = () => {
+  useEffect(() => {
+    getUsers();
+  }, []);
+
+  const getUsers = async () => {
+    try {
+      const email = await AsyncStorage.getItem('email');
+      const snapshot = await firestore()
+        .collection('users')
+        .where('email', '!=', email)
+        .get();
+
+      if (!snapshot.empty) {
+        console.log(JSON.stringify(snapshot.docs[0].data()));
+      } else {
+        console.log('No users found');
+      }
+    } catch (err) {
+      console.error('Error fetching users: ', err);
+    }
+  };
+
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>User</Text>
+      <View
+        style={{
+          alignItems: 'center',
+          backgroundColor: '#d6d6d6ff',
+          padding: 20,
+        }}
+      >
+        <Text style={styles.text}>Firebase Chat</Text>
+      </View>
     </View>
   );
 };
@@ -14,12 +46,13 @@ export default User;
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
+    // justifyContent: 'center',
+    // alignItems: 'center',
     backgroundColor: '#fff',
   },
   text: {
     fontSize: 22,
+    textAlign: 'center',
     fontWeight: '600',
   },
 });
